d4cc09e031fea0ec3479f03d0a974c68af5ddf03
Steven Wallace <snwallace@gmail.com>
2013-11-03 10:08:58 -0600

    Also reconnect on OpenSSL::SSL::SSLError.
    Sometimes bad writes will throw this.

--- lib/apns/core.rb
+++ lib/apns/core.rb
@@ -1,5 +1,5 @@
 # Copyright (c) 2009 James Pozdena, 2010 Justin.tv
-#  
+#
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
 # files (the "Software"), to deal in the Software without
@@ -8,10 +8,10 @@
 # copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following
 # conditions:
-#  
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-#  
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -226,7 +226,7 @@ module APNS
         ssl.close
         sock.close
       end
-    rescue Errno::ECONNABORTED, Errno::EPIPE, Errno::ECONNRESET
+    rescue Errno::ECONNABORTED, Errno::EPIPE, Errno::ECONNRESET, OpenSSL::SSL::SSLError
       if (retries += 1) < 5
         self.remove_connection(host, port)
         retry
