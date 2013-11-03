c25dbda37b642b9de07924ee2c1f7bca703f3d54
Steven Wallace <snwallace@gmail.com>
2013-11-03 09:52:11 -0600

    Removed default pem.

--- lib/apns/core.rb
+++ lib/apns/core.rb
@@ -1,5 +1,5 @@
 # Copyright (c) 2009 James Pozdena, 2010 Justin.tv
-#  
+#
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
 # files (the "Software"), to deal in the Software without
@@ -8,10 +8,10 @@
 # copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following
 # conditions:
-#  
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-#  
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -39,7 +39,7 @@ module APNS
   @feedback_port = 2196
 
   # openssl pkcs12 -in mycert.p12 -out client-cert.pem -nodes -clcerts
-  @pem = '/Users/robsiwek/Documents/cert.pem' # this should be the path of the pem file not the contentes
+  @pem = nil # this should be the path of the pem file not the contents
   @pass = nil
 
   @cache_connections = false
